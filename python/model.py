476bfc70c201a1a97755abdd937207ed8acb701b
Daniel <dkblackley@dundee.ac.uk>
2020-12-05 12:53:32 +0000

    Created efficient Net Model

---
+++ python/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as TF
+from efficientnet_pytorch import EfficientNet
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(Classifier, self).__init__()
+        self.model = EfficientNet.from_name("efficientnet-b0")
+        self.dropout = dropout
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.output_layer = nn.Linear(1280, 9)
+
+    def forwad(self, input):
+
+        output = self.model.extract_features(input)
+        output = self.pool(output)
+        output = output.view(output.shape[0], -1)
+
+        if self.dropout:
+            output = TF.dropout(output, self.dropout)
+
+        output = self.output_layer(output)
+        return output
