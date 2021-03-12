Added in Weight Regularisation, Batch Normalisation and fixed a bug with a missing relu layer

python/main.py
@@ -145,6 +145,16 @@ data_plot = data_plotting.DataPlotting(UNKNOWN_CLASS, test_data, test_indexes)
 
 weights = [3188, 8985, 2319, 602, 1862, 164, 170, 441]
 
+summed = sum(weights)
+new_weights = 164 / torch.Tensor(weights)
+
+"""new_weights = [weight/sum(weights) for weight in weights]
+print(new_weights)"""
+
+#new_weights = [1.0 / weight for weight in weights]
+#new_weights = new_weights / sum(new_weights)
+print(new_weights)
+
 new_weights = []
 index = 0
 for weight in weights:
@@ -154,16 +164,20 @@ for weight in weights:
         new_weights.append(sum(weights)/(8 * weight))
     index = index + 1
 
+new_weights = torch.Tensor(new_weights)
+print(new_weights)
 #weights = [4522, 12875, 3323, 867, 2624, 239, 253, 628]
 
 #new_weights = helper.apply_cost_matrix()
 
-class_weights = torch.Tensor(new_weights).to(device)
+class_weights = new_weights.to(device)
 if COST_MATRIX:
     loss_function = nn.CrossEntropyLoss(weight=class_weights, reduction='none')
 else:
     loss_function = nn.CrossEntropyLoss(weight=class_weights)
 
+
+    
 if UNKNOWN_CLASS:
     network = model.Classifier(image_size, 7, class_weights, device, dropout=0.5, BBB=BBB)
     network.to(device)
@@ -173,12 +187,14 @@ else:
     network.to(device)
     weights = [3188, 8985, 2319, 602, 1862, 164, 170, 441]
 
-optim = optimizer.Adam(network.parameters(), lr=0.001)
+optim = optimizer.Adam(network.parameters(), lr=0.001, weight_decay=0.001)
 
 #weights = list(helper.count_classes(train_set, BATCH_SIZE).values())
 
 
-
+"""output_hook = model.OutputHook()
+network.relu.register_forward_hook(output_hook)
+activation_penalty = 0.001"""
 
 
 def train(current_epoch, val_losses, train_losses, val_accuracy, train_accuracy, verbose=False):
@@ -236,15 +252,12 @@ def train(current_epoch, val_losses, train_losses, val_accuracy, train_accuracy,
                 efficientNet_loss = loss_function(outputs, label_batch)
 
                 loss = network.BBB_loss + efficientNet_loss
-                loss.backward()
-                optim.step()
 
             else:
                 outputs = network(image_batch, dropout=True)
                 loss = loss_function(outputs, label_batch)
 
-                loss.backward()
-                optim.step()
+                
 
             """if COST_MATRIX:
                 loss_values = loss_function(outputs, label_batch)
@@ -263,7 +276,16 @@ def train(current_epoch, val_losses, train_losses, val_accuracy, train_accuracy,
 
                 loss = temp_loss/(weighting)
                 else:"""
-
+            
+            """activation_cost = 0
+            for output in output_hook:
+                activation_cost += torch.norm(output, 1)
+            activation_cost *= activation_penalty
+            
+            loss += activation_cost"""
+            
+            loss.backward()
+            optim.step()
             percentage = (i_batch / len(train_set)) * 100  # Used for Debugging
 
             losses.append(loss.item())
@@ -400,7 +422,14 @@ def test(testing_set, verbose=False):
                     loss = temp_loss / (weighting)
                 else:
                     loss = loss_function(outputs, label_batch)"""
-
+            
+            """activation_cost = 0
+            for output in output_hook:
+                activation_cost += torch.norm(output, 1)
+            activation_cost *= activation_penalty
+            
+            loss += activation_cost"""
+            
             losses.append(loss.item())
 
             index = 0
@@ -469,7 +498,7 @@ def load_net(root_dir, output_size):
     train_losses = helper.read_csv(root_dir + "train_losses.csv")
     val_accuracies = helper.read_csv(root_dir + "val_accuracies.csv")
     train_accuracies = helper.read_csv(root_dir + "train_accuracies.csv")
-    network, optim = helper.load_net(root_dir + "model_parameters", image_size, output_size, class_weights)
+    network, optim = helper.load_net(root_dir + "model_parameters", image_size, output_size, device, class_weights)
 
     network = network.to(device)
 
@@ -655,6 +684,7 @@ for i in range(0, 10):
         SAVE_DIR = f"saved_models/BBB_Classifier_{i}/"
     else:
         SAVE_DIR = f"saved_models/Classifier_{i}/"
+    
 
     train_net(SAVE_DIR, 
               starting_epoch=0,
@@ -663,7 +693,7 @@ for i in range(0, 10):
               val_accuracies=[],
               train_accuracies=[])
 
-    SAVE_DIR = SAVE_DIR + "best_model/"
+    SAVE_DIR = SAVE_DIR + "best_loss/"
 
     network, optim, starting_epoch, val_losses, train_losses, val_accuracies, train_accuracies = load_net(SAVE_DIR, 8)
 

python/model.py
@@ -8,6 +8,13 @@ from efficientnet_pytorch import EfficientNet
 import BayesModel
 
 
+class OutputHook(list):
+    """ Hook to capture module outputs.
+    """
+    def __call__(self, module, input, output):
+        self.append(output)
+        
+
 class Classifier(nn.Module):
     """
     Class that holds and runs the efficientnet CNN
@@ -25,6 +32,12 @@ class Classifier(nn.Module):
         self.BBB = BBB
         self.class_weights = class_weights
         self.device = device
+        #self.relu = torch.nn.LeakyReLU()
+        self.relu = torch.nn.ReLU()
+
+        
+        size_1 = 256
+        #size_2 = 512
 
         with torch.no_grad():
 
@@ -32,15 +45,18 @@ class Classifier(nn.Module):
             encoder_size = self.model.extract_features(temp_input).shape[1]
 
         # Initialises the classification head for generating predictions.
-        self.efficient_net_output = nn.Linear(encoder_size, 512)
-
+        self.efficient_net_output = nn.Linear(encoder_size, size_1)
+        
+        self.efficient_net_batch = nn.BatchNorm1d(num_features=size_1)
+        self.bn1 = nn.BatchNorm1d(num_features=size_1)
+        
         if BBB:
-            self.hidden_layer = BayesModel.BayesianLayer(512, 512, device)
-            self.hidden_layer2 = BayesModel.BayesianLayer(512, 512, device)
+            self.hidden_layer = BayesModel.BayesianLayer(size_1, size_1, device)
+            #self.hidden_layer2 = BayesModel.BayesianLayer(512, 512, device)
         else:
-            self.hidden_layer = nn.Linear(512, 512)
-            self.hidden_layer2 = nn.Linear(512, 512)
-        self.output_layer = nn.Linear(512, output_size)
+            self.hidden_layer = nn.Linear(size_1, size_1)
+            #self.hidden_layer2 = nn.Linear(512, 512)
+        self.output_layer = nn.Linear(size_1, output_size)
 
     def forward(self, input, labels=None, sample=False, dropout=False):
         """
@@ -55,18 +71,18 @@ class Classifier(nn.Module):
         output = output.view(output.shape[0], -1)
 
         if self.training:
-            output = TF.dropout(output, self.drop_rate)
-            output = TF.relu(self.efficient_net_output(output))
+            #output = TF.dropout(output, self.drop_rate)
+            output = self.relu(self.efficient_net_batch(self.efficient_net_output(output)))
             output = TF.dropout(output, self.drop_rate)
         else:
-            output = self.efficient_net_output(output)
+            output = self.relu(self.efficient_net_batch(self.efficient_net_output(output)))
 
 
         if dropout:
-            output = TF.relu(self.hidden_layer(output))
+            output = self.relu(self.bn1(self.hidden_layer(output)))
             output = TF.dropout(output, self.drop_rate)
-            output = TF.relu(self.hidden_layer2(output))
-            output = TF.dropout(output, self.drop_rate)
+            #output = self.relu(self.hidden_layer2(output))
+            #output = TF.dropout(output, self.drop_rate)
 
         elif self.BBB:
 
@@ -74,29 +90,29 @@ class Classifier(nn.Module):
                 output = self.sample_elbo(output, labels)
                 return output
             else:
-                output = TF.relu(self.hidden_layer(output))
-                output = TF.relu(self.hidden_layer2(output))
+                output = self.relu(self.bn1(self.hidden_layer(output)))
+                #output = self.relu(self.hidden_layer2(output))
 
         else:
-            output = TF.relu(self.hidden_layer(output))
-            output = TF.relu(self.hidden_layer2(output))
+            output = self.relu(self.bn1(self.hidden_layer(output)))
+            #output = self.relu(self.hidden_layer2(output))
 
         output = self.output_layer(output)
         return output
 
     def bayesian_sample(self, input):
-        output = TF.relu(self.hidden_layer(input))
-        output = TF.relu(self.hidden_layer2(output))
+        output = self.relu(self.hidden_layer(input))
+        #output = self.relu(self.hidden_layer2(output))
         output = self.output_layer(output)
 
         return output
 
     #Methods for BBB
     def log_prior(self):
-        return self.hidden_layer.log_prior + self.hidden_layer2.log_prior
+        return self.hidden_layer.log_prior# + self.hidden_layer2.log_prior
 
     def log_variational_posterior(self):
-        return self.hidden_layer.log_variational_posterior + self.hidden_layer2.log_variational_posterior
+        return self.hidden_layer.log_variational_posterior# + self.hidden_layer2.log_variational_posterior
 
     def sample_elbo(self, input, target, samples=10, n_classes=8):
         